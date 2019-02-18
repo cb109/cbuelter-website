e2b46798379140f14dd221395d0cd5fb3f4f713c
Christoph Buelter <buelter.christoph@gmail.com>
2019-02-18 22:35:05 +0100

    Initial commit.Ã

---
+++ website/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '2-jpk0kc+2^x@0q@ksiwx8$9(3jgibxwj8=2n3+2#yrwzakzmh'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
